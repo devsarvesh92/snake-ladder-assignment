78f2130045a9dc18dc5cf5c58d52dd00fa9335d3
Sarvesh Sawant <sarveshdev92@gmail.com>
2021-10-31 01:09:08 +0530

    Create empty .net core console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadderAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
