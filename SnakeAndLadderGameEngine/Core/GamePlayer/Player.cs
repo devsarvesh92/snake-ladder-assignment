ee43e19119ba3c7598047e961fd6c859ae9125b9
Sarvesh Sawant <sarveshdev92@gmail.com>
2021-11-01 23:23:59 +0530

    Modify Player.cs for exception handling

--- SnakeAndLadderGameEngine/Core/GamePlayer/Player.cs
+++ SnakeAndLadderGameEngine/Core/GamePlayer/Player.cs
@@ -6,17 +6,26 @@ namespace SnakeLadder.Core.GamePlayer
     public class Player
     {
         public string Name { get; set; }
-        
+
         public int Position { get; set; }
-        
+
         public Player(string name, int position)
         {
             this.Name = name;
             this.Position = position;
         }
 
-        public int Play(Dice dice) => dice.Roll();
-
+        public int Play(Dice dice)
+        {
+            if (dice != null)
+            {
+                return dice.Roll();
+            }
+            else
+            {
+                throw new ArgumentNullException("dice", "Please select a dice in order for continue playing");
+            }
+        }
     }
 
 }
