bb6e7bb68a5bb9bcb758f4e8fc3f2b0dfd207cd0
Sarvesh Sawant <sarveshdev92@gmail.com>
2021-11-04 00:22:18 +0530

    Add snake from 14 to 7 to gameboard
    Player is not yet bitten by snake or snake is not yet rendered on gameboard

--- SnakeAndLadderGameEngine/Core/GameAssets/GameBoard.cs
+++ SnakeAndLadderGameEngine/Core/GameAssets/GameBoard.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SnakeLadder.Core.GameAssets
 {
     public record GameBoard(int Width, int Height)
     {
         public int Destination = Width * Height;
+        
+        public readonly List<Snake> Snakes = GetSnakes();
+
+        public Snake GetSnake(int location) => Snakes.FirstOrDefault(snake => snake.tailEnd == location);
+
+        private static List<Snake> GetSnakes()
+        {
+            return new List<Snake>()
+            {
+                new Snake(14,7)
+            };
+        }
     }
 }
---
+++ SnakeAndLadderGameEngine/Core/GameAssets/Snake.cs
@@ -0,0 +1,4 @@
+namespace SnakeLadder.Core.GameAssets
+{
+    public record Snake(int headStart, int tailEnd);
+}
