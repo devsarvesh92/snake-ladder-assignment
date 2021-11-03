d7c8d6a1724e1ab12d6e33758d0c3a780e4c9aa1
Sarvesh Sawant <sarveshdev92@gmail.com>
2021-11-04 01:11:15 +0530

    Add snake bite behaviour and unit test cases

---
+++ SnakeLadderGameEngine.Tests/SnakeTests.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SnakeLadder.Core.GameAssets;
+using SnakeLadder.Core.GamePlayer;
+using Xunit;
+
+namespace SnakeLadderGameEngine.Tests
+{
+    public class SnakeTests
+    {
+
+        [Theory]
+        [MemberData(nameof(TestDataForPlayerShouldMoveFrom14to7OnSnakeBitr))]
+
+        public void Bite_PlayerShouldMoveFrom14to7(Game game)
+        {
+            //Arrange
+            var actual = 7;
+
+            //Act
+            game.Player.Move(14);
+            var snakePresentAtlocation = game.GameBoard.Snakes.FirstOrDefault(snake => snake.headStart == game.Player.Position);
+            snakePresentAtlocation.Bite(game.Player);
+            var expected = game.Player.Position;
+
+            //Assert
+            Assert.Equal(expected,actual);
+        }
+
+        #region Snake Test TestData
+        public static IEnumerable<object[]> TestDataForPlayerShouldMoveFrom14to7OnSnakeBitr =>
+        new List<object[]>
+        {
+            new object[] { new Game(new Player("Sarvesh"),new Die()) },
+        };
+        #endregion
+    }
+}
